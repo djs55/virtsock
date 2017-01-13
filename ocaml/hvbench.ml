c6dc728c1f2b652692725bb37a5546750527a99c
David Scott <dave@recoil.org>
2017-01-13 15:15:37 +0000

    ocaml: add skeletons for client, client_conn and server
    These match the signatures of the c/ counterparts.
    
    Signed-off-by: David Scott <dave.scott@docker.com>

--- ocaml/hvbench.ml
+++ ocaml/hvbench.ml
@@ -15,13 +15,35 @@ type test =
   | Latency
   | Connection
 
-let mode = ref Server
-let test = ref Bandwidth
-let msize = ref 1024
-let use_poll = ref false
-let verbose = ref false
+let bm_guid = "3049197C-9A4E-4FBF-9367-97F792F16994"
+
+(* There's anecdotal evidence that a blocking send()/recv() is slower
+ * than performing non-blocking send()/recv() calls and then use
+ * epoll()/WSAPoll().  This flags switches between the two
+ *)
+let opt_poll = ref false
+
+(* Use a static buffer for send and receive. *)
+let buf = Cstruct.create (2 * 1024 * 1024)
+
+(* Time (in ns) to run eeach bandwidth test *)
+let bm_bw_time = 10 * 1000 * 1000 * 1000
+
+(* How many connections to make *)
+let bm_conns = 2000
+
+let server _opt_bm _opt_msgsz =
+  failwith "Server unimplemented"
+let client_conn _target =
+  failwith "Client unimplemented"
+let client _target _opt_bm _opt_msgsz =
+  failwith "Client unimplemented"
 
 let _ =
+  let mode = ref Server in
+  let test = ref Bandwidth in
+  let msize = ref 1024 in
+  let verbose = ref false in
   Arg.parse [
     "-s", Arg.Unit (fun () -> mode := Server), "Server mode";
     "-c", Arg.String (
@@ -34,9 +56,14 @@ let _ =
     "-L", Arg.Unit (fun () -> test := Latency), "Latency test";
     "-C", Arg.Unit (fun () -> test := Connection), "Connection test";
     "-m", Arg.Set_int msize, "Message size in bytes";
-    "-p", Arg.Set use_poll, "Use poll instead of blocking send()/recv()";
+    "-p", Arg.Set opt_poll, "Use poll instead of blocking send()/recv()";
     "-v", Arg.Set verbose, "Verbose output"
   ] (fun unexpected_arg ->
     Printf.fprintf stderr "Unexpected argument: %s\nSee -help for usage\n" unexpected_arg;
     exit 1
   ) "hvbench: a Hyper-V socket benchmarking program";
+
+  match !mode, !test with
+  | Server, opt_bm -> server opt_bm (!msize)
+  | Client target, Connection -> client_conn target
+  | Client target, opt_bm -> client target opt_bm (!msize)
